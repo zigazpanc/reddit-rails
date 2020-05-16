7e4ed0a3e7ab4329891379c751197cd181f70881
zigazpanc <54882393+zigazpanc@users.noreply.github.com>
2020-05-16 02:57:26 +0200

    db

--- app/models/account.rb
+++ app/models/account.rb
@@ -12,27 +12,10 @@ class Account < ApplicationRecord
   has_many :votes
 
 
-  def self.from_omniauth(access_token)
-    data = access_token.info
-    account = Account.where(email: data['email']).first
-    unless account
-      account = Account.create(username: data['name'],
-      email: data['email'],
-      password: Devise.friendly_token[0,20]
-    )
-  end
-end
-
-
-
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |account|
+  def self.create_from_provider_data(provider_data)
+    where(provider: provider_data.provider, uid: provider_data.uid).first_or_create do | account |
       account.email = provider_data.info.email
       account.password = Devise.friendly_token[0, 20]
-      account.first_name = provider_data.info.name   # assuming the user model has a name
-      account.last_name = ""
-      account.username = provider_data.info.name
     end
   end
 
