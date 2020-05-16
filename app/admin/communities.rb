011c614e65b92628bf6ee17af65c708d278ac57b
zigazpanc <54882393+zigazpanc@users.noreply.github.com>
2020-05-16 13:01:26 +0200

    fasfs

---
+++ app/admin/communities.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Community do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :account_id, :name, :url, :rules, :total_members, :summary
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:account_id, :name, :url, :rules, :total_members, :summary]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
